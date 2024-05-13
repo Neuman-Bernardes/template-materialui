feb0a82e80a34a347a46f79637b3b5fa79f4080b
Neuman <neumanbernardes@gmail.com>
2024-05-13 16:55:28 -0300

    template-materialui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+import CriarTarefa from "./pages/tarefa/CriarTarefa"; 
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CriarTarefa />
+    </div>
+  );
+}
+
+export default App;
